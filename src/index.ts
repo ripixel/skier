f97b8385e04ac30991d012deb71b446e88995dfd
James King <ripixel@gmail.com>
2025-07-14 21:26:47 +0100

    fix: expose builtins via index

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export * from './builtins/cleanAndCreateOutputTask';
+export * from './builtins/processCssTask';
+export * from './builtins/copyAssetsTask';
+export * from './builtins/readMarkdownTask';
+export * from './builtins/setGlobalsTask';
+export * from './builtins/generateItemisedTask';
+export * from './builtins/generateRssFeedTask';
+export * from './builtins/generateHtmlTask';
+export * from './builtins/generateSitemapTask';
+export * from './builtins/generateChangelogTask';
+// Add any new built-ins here as needed
